bdbbe9fc0da0909437637bf52d48ab3c69df3bd9
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-27 17:36:39 -0500

    commenting

--- ajax.js
+++ ajax.js
@@ -7,25 +7,37 @@ function loadPresentations() {
 			showPresentations, "json");
 }
 
+
+// Templating function
 var global_presentations;
 function showPresentations(pres) {
 	// console.log(pres);
 	global_presentations = pres;
 	var i, len = pres.length;
 	var presentations = $('<div>').addClass('presentations');
-	for (i=0; i< len; i++) {
-		var presentation = pres[i];
+	for (i=0; i< len; i++) {		
+
+		// make a clone of the html template for each presentation
 		var clone = $('#presentation-template > .presentation').clone();
-		clone.find('.title').html(presentation.title);
-		clone.find('.authors').html(presentation.presenterNames);
-		clone.find('.location').html(presentation.location);
-		clone.find('.time').html(presentation.start_time);
+		clone.find('.title').html(pres[i].title);
+		clone.find('.authors').html(pres[i].presenterNames);
+		clone.find('.location').html(pres[i].location);
+		clone.find('.time').html(pres[i].start_time);
+		
+		// make sure to add pid so that when div is clicked on,
+		// modal is populated with that presentation's div
 		clone.attr('pid', presentation.pid);
+		
+		// add to big presentations class
 		presentations.append(clone);
 	}
+	
+	//finally, add to presentation container placed in ajax.html
 	$('#pres-container').append(presentations);
 }
 
+// Helper method to help find which presentation we clicked on.
+// This will be used to fill the modal with the correct description
 function findPresentationWithPid(pid) {
 	for (i=0; i<global_presentations.length;i++) {
 		if ((global_presentations[i].pid) == (pid)) {
@@ -36,12 +48,14 @@ function findPresentationWithPid(pid) {
 	return null;
 }
 
+// NOTE: Because of the css of the modal, it made more sense to keep the
+// built in "exit strategy" - rather than clicking on the modal for it to disappear,
+// this modal disappears when you click on anything BUT the modal.
 $("#pres-container").click(function(event) {
 	var pid = $(event.target).closest("div.presentation").attr('pid');
 	var pres = findPresentationWithPid(pid);
 	$('#dialog').find('.title').html(pres.title);
 	$('#dialog').find('.descrip').html(pres.description);
-	$('#dialog').find('.time').html(pres.start_time);
 	console.log($('#dialog'));
 	$('#dialog').modal();
 });
